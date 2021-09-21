c67256564b34ddaccc0846a1c2dcb532d64fdc51
cchave17 <carloschavezdeveloper@gmail.com>
2021-09-21 00:34:08 -0500

    file reader almost done

--- crypto_expense_tracker/file_reader.py
+++ crypto_expense_tracker/file_reader.py
@@ -1,1 +1,7 @@
-print("Hello World")
+import xlrd
+import pandas as pd
+
+loc = "Sept_20_2021_Expense_Tracker.xlsx"
+
+df = pd.read_excel('D:\Personal Projects\Mining-Tracker\crypto-mining-tracker\Sept_20_2021_Expense_Tracker.xlsx')
+print(df)
